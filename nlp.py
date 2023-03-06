1a1f9bb0514352030e168ea195fb8fb30693c259
Red <3132515156@qq.com>
2023-03-06 21:31:52 +0800

    db是原始的database,sql_app下是我魔改过的，更新了nlp相关内容

--- nlp.py
+++ nlp.py
@@ -1,18 +1,62 @@
-import jsonlines
-import time
-import jionlp as jio
 from typing import List
 
-def nlp(log_file):
-    with open(log_file, 'r+') as f:
-        for item in jsonlines.Reader(f):
-            content = item['message']
-            try:
-                print(jio.parse_time(content, time_base=time.time()))
-            except:
-                print("\"" + content + "\"" + " " + "have no symbol of time")
+class Time_msg(object):
+    def __init__(self,group,text,type,time,content):
+        self.__group = group
+        self.__text = text
+        self.__type = type
+        self.__time = time
+        self.__content = content
 
-nlp("QQlog.json")
+    # def get_group(self):
+    #     return self.__group
+    
+    # def get_type(self):
+    #     return self.__type
+    
+    # def get_text(self):
+    #     return self.__text
+    
+    # def get_time(self):
+    #     return self.__time
+    
+    # def get_content(self):
+    #     return self.__content
+    
+    # def set_group(self,group):
+    #     self.__group = group
+    
+    # def set_type(self, type):
+    #     self.__type = type
+
+    # def set_text(self, text):
+    #     self.__text = text
+
+    # def set_time(self, time):
+    #     self.__time = time
+
+    # def set_content(self, content):
+    #     self.__content = content
+
+    
+
+
+
+def nlp(data:List,group,content):
+    for dt in data:
+        try:
+            gr = group
+            tx = dt["text"]
+            tp = dt["type"]
+            t = dt["detail"]["time"]
+            t_msg = Time_msg(gr,tx,tp,t,content)
+
+            print(t_msg)
+        except:
+            print("error")
+
+
+
 
 
 
